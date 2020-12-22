2de706bda880f60929ccbe8f2f23599b0d26c20a
w_jiangtao <w_jiangtao1120@163.com>
2020-12-22 23:17:06 +0800

    江涛

--- src/com/dianju/crm/tool/Test.java
+++ src/com/dianju/crm/tool/Test.java
@@ -2,6 +2,10 @@ package com.dianju.crm.tool;
 
 public class Test {
     public static void main(String[] args) {
+
         System.out.print("m1");
+        System.out.print("江涛1");
+        System.out.print("江涛2");
+        System.out.print("江涛3");
     }
 }
