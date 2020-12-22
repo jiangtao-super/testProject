8624299a01c029ab2f9e46c3c4bd2bc5f25dfc4d
w_jiangtao <w_jiangtao1120@163.com>
2020-12-22 23:15:54 +0800

    m2

--- src/com/dianju/crm/tool/Test.java
+++ src/com/dianju/crm/tool/Test.java
@@ -3,5 +3,8 @@ package com.dianju.crm.tool;
 public class Test {
     public static void main(String[] args) {
         System.out.print("m1");
+        System.out.print("jiangtao1");
+        System.out.print("jiangtao2");
+        System.out.print("m2");
     }
 }
